2c1bba9cce3bb39b5e27373cb5a118370e78cf1e
Matleftod <97225801+Matleftod@users.noreply.github.com>
2023-04-19 14:04:26 +0200

    add editing

--- to-do-list/src/components/TodoItem.vue
+++ to-do-list/src/components/TodoItem.vue
@@ -1,30 +1,62 @@
 <template>
-    <div>
-      <input type="checkbox" :checked="todo.selected" @change="$emit('toggle-selected', todo)" />      
-      <span>{{ todo.title }}</span>
-      <span>{{ todo.estimatedHours }} heures</span>
-      <span>{{ responsibleName }}</span>
-      <button @click="$emit('edit', todo)">Éditer</button>
-      <button @click="$emit('delete', todo)">Supprimer</button>
+  <div>
+    <div v-if="isEditing">
+      <input v-model="localTodo.title" @keyup.enter="saveChanges" placeholder="Title" />
+      <input type="number" v-model="localTodo.estimatedHours" @keyup.enter="saveChanges" placeholder="Estimated hours" />
+      <select v-model="localTodo.responsible">
+        <option v-for="user in users" :key="user.id" :value="user.id">
+          {{ user.name }}
+        </option>
+      </select>
+      <button @click="saveChanges">Save</button>
+      <button @click="cancelEditing">Cancel</button>
     </div>
-  </template>
+    <div v-else>
+      <input type="checkbox" :checked="localTodo.selected" @change="$emit('toggle-selected', localTodo)" />
+      <span>{{ localTodo.title }}</span>
+      <span>{{ localTodo.estimatedHours }} heures</span>
+      <span>{{ responsibleName }}</span>
+      <button @click="startEditing">Edit</button>
+      <button @click="$emit('delete', localTodo)">Delete</button>
+    </div>
+  </div>
+</template>
   
-  <script>
-  import { mapState } from 'vuex';
+<script>
+import { mapState } from 'vuex';
   
-  export default {
-    props: {
-      todo: {
-        type: Object,
-        required: true,
-      },
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
     },
-    computed: {
-      ...mapState(['users']),
-      responsibleName() {
-        const responsible = this.users.find(user => user.id === this.todo.responsible);
-        return responsible ? responsible.name : '';
-      },
+  },
+  data() {
+    return {
+      isEditing: false,
+      localTodo: { ...this.todo },
+    };
+  },
+  computed: {
+    ...mapState(['users']),
+    responsibleName() {
+      const responsible = this.users.find(user => user.id === this.todo.responsible);
+      return responsible ? responsible.name : '';
     },
-  };
-  </script>  
+  },
+  methods: {
+    startEditing() {
+      this.isEditing = true;
+      this.localTodo = { ...this.todo };
+    },
+    saveChanges() {
+      this.$emit('edit', this.localTodo);
+      this.isEditing = false;
+    },
+    cancelEditing() {
+      this.isEditing = false;
+    },
+  },
+};
+</script>  
